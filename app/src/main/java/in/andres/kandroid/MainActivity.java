a85e0082a5b59f00d4114bba4079369d0a577e55
Thomas Andres <thomas@andres.in>
2017-01-01 15:07:46 +0100

    Added images for drawer menu New app icon Some translations

--- app/src/main/java/in/andres/kandroid/MainActivity.java
+++ app/src/main/java/in/andres/kandroid/MainActivity.java
@@ -225,15 +225,16 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_refresh) {
+//        if (id == R.id.nav_camera) {
+//            // Handle the camera action
+//        } else if (id == R.id.nav_gallery) {
+//
+//        } else if (id == R.id.nav_slideshow) {
+//
+//        } else if (id == R.id.nav_manage) {
+//
+//        } else
+        if (id == R.id.nav_refresh) {
 
         } else if (id == R.id.nav_sign_in) {
             Intent iLoginScreen = new Intent(this, LoginActivity.class);
