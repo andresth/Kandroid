c11da37d0302ea377324ef7ae2876063f8798c5f
Thomas Andres <thomas@andres.in>
2017-01-02 21:20:22 +0100

    AboutActivity: Return button in action bar

--- app/src/main/java/in/andres/kandroid/AboutActivity.java
+++ app/src/main/java/in/andres/kandroid/AboutActivity.java
@@ -1,8 +1,10 @@
 package in.andres.kandroid;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.widget.TextView;
 
@@ -11,6 +13,26 @@ public class AboutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setupActionBar();
         setContentView(R.layout.activity_about);
+    }
+
+    private void  setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            // Show the Up button in the action bar.
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            onBackPressed();
+//            NavUtils.navigateUpFromSameTask(this);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
