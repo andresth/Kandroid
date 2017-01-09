23f49c287797929b8843b58fc6288891c18224c6
Thomas Andres <thomas@andres.in>
2017-01-09 19:07:53 +0100

    More details on Activities

--- app/src/main/java/in/andres/kandroid/DashActivitiesFragment.java
+++ app/src/main/java/in/andres/kandroid/DashActivitiesFragment.java
@@ -1,11 +1,19 @@
 package in.andres.kandroid;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ListFragment;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
 
 import in.andres.kandroid.kanboard.KanboardActivity;
-import in.andres.kandroid.kanboard.KanboardTask;
 
 /**
  * Created by Thomas Andres on 07.01.17.
@@ -23,8 +31,34 @@ public class DashActivitiesFragment extends ListFragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         if (((MainActivity)getActivity()).getDashboard() != null) {
-            ArrayAdapter<KanboardActivity> listAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, ((MainActivity)getActivity()).getDashboard().Activities);
+//            ArrayAdapter<KanboardActivity> listAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, ((MainActivity)getActivity()).getDashboard().Activities);
+            DashActivityAdapter listAdapter = new DashActivityAdapter(getActivity(), ((MainActivity)getActivity()).getDashboard().Activities);
             setListAdapter(listAdapter);
+        }
+    }
+
+    class DashActivityAdapter extends ArrayAdapter<KanboardActivity> {
+        private Context mContext;
+        private LayoutInflater mInflater;
+        private List<KanboardActivity> mValues;
+
+        DashActivityAdapter(Context context, List<KanboardActivity> values) {
+            super(context, android.R.layout.simple_list_item_1, values);
+            mContext = context;
+            mInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            mValues = values;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+            if (convertView == null)
+                convertView = mInflater.inflate(android.R.layout.simple_list_item_1, null);
+
+            TextView textView = (TextView) convertView.findViewById(android.R.id.text1);
+            textView.setText(Html.fromHtml(mValues.get(position).Content));
+
+            return convertView;
         }
     }
 }
