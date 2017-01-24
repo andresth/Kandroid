c8d1192ee95dcecb214f7262a257e314c0a26e48
Thomas Andres <thomas@andres.in>
2017-01-24 23:05:53 +0100

    getMe event listener

---
+++ app/src/main/java/in/andres/kandroid/kanboard/OnGetMeListener.java
@@ -0,0 +1,10 @@
+package in.andres.kandroid.kanboard;
+
+/**
+ * Created by Thomas Andres on 24.01.17.
+ */
+
+public interface OnGetMeListener {
+    void onGetMe(boolean success, KanboardUserInfo result);
+
+}
