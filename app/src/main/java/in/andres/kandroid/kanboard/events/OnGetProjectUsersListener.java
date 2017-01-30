29a7dbea082535f3bc0153ced5fbac9dd759ca05
Thomas Andres <thomas@andres.in>
2017-01-30 20:34:12 +0100

    Changed OnGetProjectUsers event to Dictionary

--- app/src/main/java/in/andres/kandroid/kanboard/events/OnGetProjectUsersListener.java
+++ app/src/main/java/in/andres/kandroid/kanboard/events/OnGetProjectUsersListener.java
@@ -1,11 +1,11 @@
 package in.andres.kandroid.kanboard.events;
 
-import java.util.Hashtable;
+import java.util.Dictionary;
 
 /**
  * Created by Thomas Andres on 20.01.17.
  */
 
 public interface OnGetProjectUsersListener {
-    void onGetProjectUsers(boolean success, Hashtable<Integer, String> result);
+    void onGetProjectUsers(boolean success, Dictionary<Integer, String> result);
 }
