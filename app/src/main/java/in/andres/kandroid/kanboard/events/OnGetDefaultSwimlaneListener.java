ca580dd0b15222fb269efb8abbf2525d18c3bad1
Thomas Andres <thomas@andres.in>
2017-02-01 23:34:57 +0100

    Changed the result of getDefaultSwimlane to match getSwimlane

--- app/src/main/java/in/andres/kandroid/kanboard/events/OnGetDefaultSwimlaneListener.java
+++
@@ -1,9 +0,0 @@
-package in.andres.kandroid.kanboard.events;
-
-/**
- * Created by Thomas Andres on 21.01.17.
- */
-
-public interface OnGetDefaultSwimlaneListener {
-    void onGetDefaultSwimlane(boolean success, String name, boolean isActive);
-}
