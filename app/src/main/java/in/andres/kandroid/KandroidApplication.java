8bce3b08e68e383ced9598da82973dc563ba3cd1
Thomas Andres <thomas@andres.in>
2017-02-11 17:48:51 +0100

    More options for ACRA

--- app/src/main/java/in/andres/kandroid/KandroidApplication.java
+++ app/src/main/java/in/andres/kandroid/KandroidApplication.java
@@ -12,8 +12,9 @@ import org.acra.annotation.*;
  */
 
 @ReportsCrashes(mailTo = "kandroid@andres.in",
-                customReportContent = { ReportField.APP_VERSION_CODE, ReportField.APP_VERSION_NAME, ReportField.ANDROID_VERSION, ReportField.PHONE_MODEL, ReportField.CUSTOM_DATA, ReportField.STACK_TRACE, ReportField.LOGCAT },
+                customReportContent = { ReportField.APP_VERSION_CODE, ReportField.APP_VERSION_NAME, ReportField.ANDROID_VERSION, ReportField.PHONE_MODEL, ReportField.CUSTOM_DATA, ReportField.USER_COMMENT, ReportField.STACK_TRACE, ReportField.LOGCAT },
                 mode = ReportingInteractionMode.DIALOG,
+                logcatArguments = { "-t", "200", "-v", "time", "Kandroid:d", "InstantRun:s", "*:e" },
                 resDialogIcon = android.R.drawable.stat_notify_error,
                 resDialogTitle = R.string.acra_dialog_title,
                 resDialogText = R.string.acra_dialog_text,
