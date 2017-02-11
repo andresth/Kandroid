30409dd5266a8edefa58fa920a5313170b73bdac
Thomas Andres <thomas@andres.in>
2017-02-11 21:50:23 +0100

    Upload crash reports to server

--- app/src/main/java/in/andres/kandroid/KandroidApplication.java
+++ app/src/main/java/in/andres/kandroid/KandroidApplication.java
@@ -6,19 +6,24 @@ import android.util.Log;
 
 import org.acra.*;
 import org.acra.annotation.*;
+import org.acra.sender.HttpSender;
 
 /**
  * Created by thomas on 11.02.17.
  */
 
-@ReportsCrashes(mailTo = "kandroid@andres.in",
-                customReportContent = { ReportField.APP_VERSION_CODE, ReportField.APP_VERSION_NAME, ReportField.ANDROID_VERSION, ReportField.PHONE_MODEL, ReportField.CUSTOM_DATA, ReportField.USER_COMMENT, ReportField.STACK_TRACE, ReportField.LOGCAT },
+@ReportsCrashes(//mailTo = "kandroid@andres.in",
+                formUri = "https://uber.andres.in/crashreport/report.php",
+                //customReportContent = { ReportField.APP_VERSION_CODE, ReportField.APP_VERSION_NAME, ReportField.ANDROID_VERSION, ReportField.PHONE_MODEL, ReportField.CUSTOM_DATA, ReportField.USER_COMMENT, ReportField.STACK_TRACE, ReportField.LOGCAT },
                 mode = ReportingInteractionMode.DIALOG,
+                httpMethod = HttpSender.Method.PUT,
+                reportType = HttpSender.Type.JSON,
                 logcatArguments = { "-t", "200", "-v", "time", "Kandroid:d", "InstantRun:s", "*:e" },
                 resDialogIcon = android.R.drawable.stat_notify_error,
                 resDialogTitle = R.string.acra_dialog_title,
-                resDialogText = R.string.acra_dialog_text,
-                resDialogPositiveButtonText = R.string.acra_send_mail,
+//                resDialogText = R.string.acra_dialog_text,
+                resDialogText = R.string.acra_dialog_text_upload,
+//                resDialogPositiveButtonText = R.string.acra_send_mail,
                 resDialogCommentPrompt = R.string.acra_dialog_comment_prompt)
 public class KandroidApplication extends Application {
     @Override
