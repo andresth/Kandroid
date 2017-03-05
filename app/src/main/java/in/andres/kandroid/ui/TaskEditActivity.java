5a0c607bcfdbba3d24eedf4257dc844454f732d0
Thomas Andres <thomas@andres.in>
2017-03-05 22:15:11 +0100

    Color selection in edit task activity

--- app/src/main/java/in/andres/kandroid/ui/TaskEditActivity.java
+++ app/src/main/java/in/andres/kandroid/ui/TaskEditActivity.java
@@ -24,11 +24,15 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.preference.PreferenceManager;
+import android.support.annotation.ColorInt;
 import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -39,11 +43,14 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
 
+import com.thebluealliance.spectrum.SpectrumDialog;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
+import java.util.Dictionary;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Hashtable;
@@ -52,12 +59,14 @@ import in.andres.kandroid.Constants;
 import in.andres.kandroid.R;
 import in.andres.kandroid.Utils;
 import in.andres.kandroid.kanboard.KanboardAPI;
+import in.andres.kandroid.kanboard.KanboardColor;
 import in.andres.kandroid.kanboard.KanboardTask;
 import in.andres.kandroid.kanboard.events.OnCreateTaskListener;
+import in.andres.kandroid.kanboard.events.OnGetDefaultColorListener;
+import in.andres.kandroid.kanboard.events.OnGetDefaultColorsListener;
 import in.andres.kandroid.kanboard.events.OnUpdateTaskListener;
 
-
-public class TaskEditActivity extends AppCompatActivity {
+public class TaskEditActivity extends AppCompatActivity implements OnCreateTaskListener, OnUpdateTaskListener, OnGetDefaultColorListener, OnGetDefaultColorsListener {
     private KanboardTask task;
     private String taskTitle;
     private String taskDescription;
@@ -70,17 +79,50 @@ public class TaskEditActivity extends AppCompatActivity {
     private int columnId;
     private int ownerId;
     private int creatorId;
-    private int colorId;
+    private String colorId;
     private int projectid;
     private Hashtable<Integer, String> projectUsers;
+    private Dictionary<String, KanboardColor> kanboardColors;
+    private int[] colorArray;
+    private String defaultColor;
 
     private EditText editTextTitle;
     private EditText editTextDescription;
     private Button btnStartDate;
     private Button btnDueDate;
+    private Button btnColor;
     private EditText editHoursEstimated;
     private EditText editHoursSpent;
     private Spinner spinnerProjectUsers;
+
+    private KanboardAPI kanboardAPI;
+
+    private View.OnClickListener btnColorClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            new SpectrumDialog.Builder(getBaseContext())
+                    .setColors(colorArray)
+                    .setSelectedColor(kanboardColors.get(colorId != null ? colorId : defaultColor).getBackground())
+                    .setDismissOnColorSelected(true)
+                    .setTitle(R.string.taskedit_seclect_color)
+                    .setOnColorSelectedListener(new SpectrumDialog.OnColorSelectedListener() {
+                        @Override
+                        public void onColorSelected(boolean positiveResult, @ColorInt int color) {
+                            if (positiveResult) {
+                                Enumeration<String> iter = kanboardColors.keys();
+                                while (iter.hasMoreElements()) {
+                                    String key = iter.nextElement();
+                                    if (kanboardColors.get(key).getBackground() == color) {
+                                        colorId = key;
+                                        break;
+                                    }
+                                }
+                                setButtonColor();
+                            }
+                        }
+                    }).build().show(getSupportFragmentManager(), "color_dialog");
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -92,6 +134,9 @@ public class TaskEditActivity extends AppCompatActivity {
         editTextDescription = (EditText) findViewById(R.id.edit_task_description);
         btnStartDate = (Button) findViewById(R.id.btn_start_date);
         btnDueDate = (Button) findViewById(R.id.btn_due_date);
+        btnColor = (Button) findViewById(R.id.color_button);
+        btnColor.setOnClickListener(btnColorClick);
+        btnColor.setText(Utils.fromHtml(getString(R.string.taskedit_color, "")));
         editHoursEstimated = (EditText) findViewById(R.id.edit_hours_estimated);
         editHoursSpent = (EditText) findViewById(R.id.edit_hours_spent);
         spinnerProjectUsers = (Spinner) findViewById(R.id.user_spinner);
@@ -106,16 +151,30 @@ public class TaskEditActivity extends AppCompatActivity {
             timeEstimated = task.getTimeEstimated();
             timeSpent = task.getTimeSpent();
             ownerId = task.getOwnerId();
+            colorId = task.getColorId();
             setActionBarTitle(getString(R.string.taskview_fab_edit_task));
         } else {
             isNewTask = true;
             projectid = getIntent().getIntExtra("projectid", 0);
-            colorId = getIntent().getIntExtra("colorid", 0);
+//            colorId = getIntent().getIntExtra("colorid", 0);
             creatorId = getIntent().getIntExtra("creatorid", 0);
             ownerId = getIntent().getIntExtra("ownerid", 0);
             columnId = getIntent().getIntExtra("columnid",0);
             swimlaneId = getIntent().getIntExtra("swimlaneid", 0);
             setActionBarTitle(getString(R.string.taskedit_new_task));
+        }
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this.getBaseContext());
+        try {
+            kanboardAPI = new KanboardAPI(preferences.getString("serverurl", ""), preferences.getString("username", ""), preferences.getString("password", ""));
+            kanboardAPI.addOnCreateTaskListener(this);
+            kanboardAPI.addOnUpdateTaskListener(this);
+            kanboardAPI.addOnGetDefaultColorListener(this);
+            kanboardAPI.addOnGetDefaultColorsListener(this);
+            kanboardAPI.getDefaultTaskColor();
+            kanboardAPI.getDefaultTaskColors();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
         if (getIntent().hasExtra("projectusers")) {
@@ -205,46 +264,26 @@ public class TaskEditActivity extends AppCompatActivity {
                 onBackPressed();
                 return true;
             case R.id.action_save:
-                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this.getBaseContext());
-                try {
-                    KanboardAPI kanboardAPI = new KanboardAPI(preferences.getString("serverurl", ""), preferences.getString("username", ""), preferences.getString("password", ""));
-                    kanboardAPI.addOnCreateTaskListener(new OnCreateTaskListener() {
-                        @Override
-                        public void onCreateTask(boolean success, Integer taskid) {
-                            setResult(Constants.ResultChanged, new Intent());
-                            finish();
-                        }
-                    });
-                    kanboardAPI.addOnUpdateTaskListener(new OnUpdateTaskListener() {
-                        @Override
-                        public void onUpdateTask(boolean success) {
-                            setResult(Constants.ResultChanged, new Intent());
-                            finish();
-                        }
-                    });
-                    ownerId = spinnerProjectUsers.getSelectedItemPosition();
-                    if (spinnerProjectUsers.getSelectedItemPosition() != 0) {
-                        for (Enumeration<Integer> iter = projectUsers.keys(); iter.hasMoreElements();) {
-                            Integer key = iter.nextElement();
-                            if (projectUsers.get(key).contentEquals((String) spinnerProjectUsers.getSelectedItem())) {
-                                ownerId = key;
-                                break;
-                            }
+                ownerId = spinnerProjectUsers.getSelectedItemPosition();
+                if (spinnerProjectUsers.getSelectedItemPosition() != 0) {
+                    for (Enumeration<Integer> iter = projectUsers.keys(); iter.hasMoreElements();) {
+                        Integer key = iter.nextElement();
+                        if (projectUsers.get(key).contentEquals((String) spinnerProjectUsers.getSelectedItem())) {
+                            ownerId = key;
+                            break;
                         }
                     }
-                    if (isNewTask) {
-                        kanboardAPI.createTask(editTextTitle.getText().toString(), projectid, null, columnId, ownerId, null, dueDate, editTextDescription.getText().toString(), null, null, swimlaneId, null, null, null, null, null, null, null);
-
-                    } else {
-                        kanboardAPI.updateTask(task.getId(), editTextTitle.getText().toString(), null, ownerId, dueDate, editTextDescription.getText().toString(), null, null, null, null, null, null, null, null, null);
-                    }
-                    ProgressBar prog = new ProgressBar(TaskEditActivity.this);
-                    prog.getIndeterminateDrawable().setColorFilter(Color.WHITE, android.graphics.PorterDuff.Mode.MULTIPLY);
-                    item.setActionView(prog);
-                    item.expandActionView();
-                } catch (IOException e) {
-                    e.printStackTrace();
                 }
+                if (isNewTask) {
+                    kanboardAPI.createTask(editTextTitle.getText().toString(), projectid, colorId != null ? colorId : defaultColor, columnId, ownerId, null, dueDate, editTextDescription.getText().toString(), null, null, swimlaneId, null, null, null, null, null, null, null);
+
+                } else {
+                    kanboardAPI.updateTask(task.getId(), editTextTitle.getText().toString(), colorId != null ? colorId : defaultColor, ownerId, dueDate, editTextDescription.getText().toString(), null, null, null, null, null, null, null, null, null);
+                }
+                ProgressBar prog = new ProgressBar(TaskEditActivity.this);
+                prog.getIndeterminateDrawable().setColorFilter(Color.WHITE, android.graphics.PorterDuff.Mode.MULTIPLY);
+                item.setActionView(prog);
+                item.expandActionView();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -269,5 +308,55 @@ public class TaskEditActivity extends AppCompatActivity {
     private void setActionBarTitle(@NonNull String title) {
         if (getSupportActionBar() != null)
             getSupportActionBar().setTitle(title);
+    }
+
+    @Override
+    public void onCreateTask(boolean success, Integer taskid) {
+        setResult(Constants.ResultChanged, new Intent());
+        finish();
+    }
+
+    @Override
+    public void onUpdateTask(boolean success) {
+        setResult(Constants.ResultChanged, new Intent());
+        finish();
+    }
+
+    @Override
+    public void onGetDefaultColor(boolean success, String colorid) {
+        defaultColor = colorid;
+        setButtonColor();
+    }
+
+    @Override
+    public void onGetDefaultColors(boolean success, Dictionary<String, KanboardColor> colors) {
+        kanboardColors = colors;
+        colorArray = new int[kanboardColors.size()];
+        int i = 0;
+        Enumeration<String> iter = kanboardColors.keys();
+        while (iter.hasMoreElements()) {
+            String key = iter.nextElement();
+            colorArray[i] = kanboardColors.get(key).getBackground();
+            i++;
+        }
+        Log.d(Constants.TAG, colors.toString());
+        setButtonColor();
+    }
+
+    private void setButtonColor() {
+        if (kanboardColors == null || defaultColor == null)
+            return;
+
+        btnColor.setEnabled(true);
+
+        Drawable dot = getDrawable(R.drawable.shape_circle);
+        if (colorId != null) {
+            dot.setColorFilter(kanboardColors.get(colorId).getBackground(), PorterDuff.Mode.MULTIPLY);
+            btnColor.setText(Utils.fromHtml(getString(R.string.taskedit_color, kanboardColors.get(colorId).getName())));
+        } else {
+            dot.setColorFilter(kanboardColors.get(defaultColor).getBackground(), PorterDuff.Mode.MULTIPLY);
+            btnColor.setText(Utils.fromHtml(getString(R.string.taskedit_color, kanboardColors.get(defaultColor).getName())));
+        }
+        btnColor.setCompoundDrawablesRelativeWithIntrinsicBounds(dot, null, null, null);
     }
 }
